0d465fdea4c876a4a94e2d3f449569605edbd5a9
Lyn Heo <Lyn Heo>
2016-01-12 21:30:33 +0900

    Update First Version

---
+++ src/Lyn-EraseIFDEF/Main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"sync"
+)
+
+type ProcInfo struct {
+	deleteDefine   string
+	backupOriginal bool
+}
+
+func parseCommandLine() ([]string, ProcInfo, bool) {
+	fileName := flag.String("file", "", "FileName")
+	fileNameRegExp := flag.String("rfile", "", "FileName RegExp")
+	runPath := flag.String("path", "", "Running Path")
+	deleteDefine := flag.String("define", "", "Delete Define(Required)")
+	backupOriginal := flag.Bool("backup", false, "Backup Original File")
+
+	var fileList []string
+	var procInfo ProcInfo
+
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		flag.Usage()
+		return fileList, procInfo, false
+	}
+
+	if *deleteDefine == "" {
+		flag.Usage()
+		return fileList, procInfo, false
+	}
+	fmt.Println(*runPath)
+	procInfo.backupOriginal = *backupOriginal
+	procInfo.deleteDefine = *deleteDefine
+
+	if *fileNameRegExp == "" {
+		fileList = append(fileList, filepath.Join(*runPath, *fileName))
+	} else {
+		rx, result := regexp.Compile(*fileNameRegExp)
+
+		if result != nil {
+			rx = nil
+			flag.Usage()
+			fmt.Println("Regexp Syntex Error")
+			return fileList, procInfo, false
+		} else {
+			fmt.Println("File scan from ", *runPath)
+			allFiles, _ := ioutil.ReadDir(*runPath)
+			fmt.Println("FileCount : ", len(allFiles))
+
+			for _, f := range allFiles {
+				if rx.MatchString(f.Name()) == true {
+					fileList = append(fileList, filepath.Join(*runPath, f.Name()))
+					fmt.Println("ScanFile : ", f.Name(), " Ok")
+				} else {
+					fmt.Println("ScanFile : ", f.Name(), " Skip")
+				}
+			}
+		}
+	}
+
+	return fileList, procInfo, true
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	fileList, procInfo, isSuccess := parseCommandLine()
+	if isSuccess == false {
+		return
+	}
+
+	fmt.Println("Define : ", procInfo.deleteDefine)
+	wg := new(sync.WaitGroup)
+
+	for _, fileName := range fileList {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, procInfo ProcInfo, fileName string) {
+			fmt.Println("Start : ", fileName)
+			doProcessSource(fileName, procInfo)
+			fmt.Println("End : ", fileName)
+			wg.Done()
+		}(wg, procInfo, fileName)
+	}
+
+	wg.Wait()
+}
---
+++ src/Lyn-EraseIFDEF/parseFile.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"unicode"
+)
+
+const (
+	Off = iota
+	Use
+	Remove
+)
+
+func readFileLines(fileName string) []string {
+	var lines []string
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	r := bufio.NewReader(file)
+
+	for {
+		s, _, e := r.ReadLine()
+		if e != nil {
+			break
+		}
+
+		lines = append(lines, string(s))
+	}
+	return lines
+}
+
+func copyFile(original, dest string) {
+	r, err := os.Open(original)
+	if err != nil {
+		panic(err)
+	}
+
+	defer r.Close()
+
+	w, err := os.Create(dest)
+	if err != nil {
+		panic(err)
+	}
+
+	defer w.Close()
+
+	_, err = io.Copy(w, r)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func doProcessSource(fileName string, procInfo ProcInfo) {
+	if procInfo.backupOriginal == true {
+		backupFileName := fileName + ".bak"
+		copyFile(fileName, backupFileName)
+	}
+
+	lines := readFileLines(fileName)
+
+	status := Off
+
+	output := make([]string, 0, len(lines))
+	for _, line := range lines {
+		line = strings.TrimRightFunc(line, unicode.IsSpace)
+		if len(line) == 0 {
+			output = append(output, line)
+			continue
+		}
+
+		if line[0] == '#' {
+			var preprocessor string
+			var variable string
+			fmt.Sscanf(line, "%s %s", &preprocessor, &variable)
+
+			preprocessor = strings.TrimSpace(preprocessor)
+			variable = strings.TrimSpace(variable)
+
+			switch preprocessor {
+			case "#ifdef":
+				if variable == procInfo.deleteDefine {
+					status = Use
+					continue
+				}
+			case "#ifndef":
+				if variable == procInfo.deleteDefine {
+					status = Remove
+					continue
+				}
+			case "#else":
+				if status == Use {
+					status = Remove
+					continue
+				} else if status == Remove {
+					status = Use
+				}
+			case "#endif":
+				if status == Remove || status == Use {
+					status = Off
+					continue
+				}
+			}
+		}
+		switch status {
+		case Use, Off:
+			output = append(output, line)
+		}
+	}
+
+	file, err := os.Create(fileName)
+	if err == nil {
+		defer file.Close()
+		for _, line := range output {
+			file.WriteString(line)
+			file.WriteString("\n")
+		}
+	} else {
+		panic(err)
+	}
+}
